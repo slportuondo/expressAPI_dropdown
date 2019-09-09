cd08bccd732109f0bb1d774430a43b7877f76545
Sebastian Portuondo <slportuondo@gmail.com>
2019-09-09 11:13:21 -0500

    user can drag images and srcURL is stored in DB

--- controllers/dropController.js
+++ controllers/dropController.js
@@ -7,25 +7,31 @@ const fs = require('fs')
 
 const dropImage = multer({dest: 'imagesDropped/'})
 
-router.post('/', dropImage.single('image'), async (req,res,next) => {
+router.post('/', )
 
-  //Create a file in the imagesDropped folder
-	const filePath = req.file.path
+// router.post('/', dropImage.single('image'), async (req,res,next) => {
+router.post('/', async (req,res,next) => {
+	console.log('SOMEONE JUST POSTED, here\'s what');
+  // //Create a file in the imagesDropped folder
+	// const filePath = req.file.path
+
+	console.log(req.body);
   const foundOwner = await Session.findOne(req.body.id)
   const createdImage = new Image
 
   try {
-    createdImage.owner = foundOwner
-    createdImage.title = req.body.title
+    // createdImage.owner = foundOwner
+    // createdImage.title = req.body.title
     createdImage.foundOnURL = req.body.foundOnURL
-    createdImage.img.data = fs.readFileSync(filePath)
-		createdImage.img.contentType = req.file.mimetype
+    // createdImage.img.data = fs.readFileSync(filePath)
+		// createdImage.img.contentType = req.file.mimetype
 
 		await createdImage.save()
 
+		res.send(createdImage)
     // Delete the file from the imagesDropped folder
-		fs.unlinkSync(filePath)
-    res.send(createdImage)
+		// fs.unlinkSync(filePath)
+    // res.send(createdImage)
 	} catch(err){
 		next(err)
 	}
